70dadadb97d2140058161c46e7f6416391e7125e
unknown <m_fattih@hotmail.com>
2020-05-22 20:52:18 -0700

    added delete and put Stretch

--- Routers/cars-router.js
+++ Routers/cars-router.js
@@ -30,4 +30,35 @@ router.post("/", (req, res) => {
     });
 });
 
+router.put("/:id", (req, res) => {
+  const CarData = req.body;
+  const { id } = req.params;
+  console.log("car Data", CarData, "id ", id);
+  knex("cars")
+    .update(CarData)
+    .where({ id })
+    .then((car) => {
+      res.status(200).json(CarData);
+    })
+    .catch((err) => {
+      console.log("car Data error ", err);
+      res.status(500).json({ errorMessage: "Server Error", error: err });
+    });
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [deleted] = await knex("cars").where({ id });
+    const car = await knex("cars").del().where({ id });
+    if (car > 0) {
+      res.status(200).json(deleted);
+    } else {
+      res.status(404).json({ message: "can't delete the car" });
+    }
+  } catch (err) {
+    res.status(500).json({ errorMessage: "Server Error", error: err });
+  }
+});
+
 module.exports = router;
